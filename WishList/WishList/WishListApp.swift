d1ac983eb907b7960264fd89d998a8ba02fb0c03
sepideh asgharpoor hooshiar <sepideh.a.hooshiar@gmail.com>
2026-01-01 05:46:48 +0330

    - WishList project is created.

---
+++ WishList/WishList/WishListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishListApp.swift
+//  WishList
+//
+//  Created by sepideh aph on 2026-01-01.
+//
+
+import SwiftUI
+
+@main
+struct WishListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
